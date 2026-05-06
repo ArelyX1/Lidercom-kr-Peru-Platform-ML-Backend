724dbc84187966368ef781ad436f29c115f7a9d4
ArelyX1 <andrycaceres2006@gmail.com>
2026-05-06 06:00:00 -0500

    fix: encrypt

--- App/rust_services/src/mtopy/validate_identity.rs
+++ App/rust_services/src/mtopy/validate_identity.rs
@@ -1,6 +1,6 @@
 use crate::mcrypto;
 
-pub fn validate_wallet(wallet_content: &[u8], encrypted_wallet: &[u8], master_pass: &str) -> bool {
+pub fn wallet_conformance(wallet_content: &[u8], encrypted_wallet: &[u8], master_pass: &str) -> bool {
     let decrypted_wallet = mcrypto::decrypt_data(encrypted_wallet, master_pass);
     decrypted_wallet == wallet_content
 }
