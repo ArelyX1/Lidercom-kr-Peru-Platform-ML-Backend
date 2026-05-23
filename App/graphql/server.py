c7cbb5fe2e6eed9736de3f32e20f0338c598ea79
ArelyX1 <andrycaceres2006@gmail.com>
2026-05-22 20:19:33 -0500

    fix: add CORS middleware to GraphQL server

--- App/graphql/server.py
+++ App/graphql/server.py
@@ -4,8 +4,16 @@ Description: Configura el servidor ASGI de Strawberry GraphQL usando el esquema 
 """
 from strawberry.asgi import GraphQL
 from graphql.schema.index import schema
+from starlette.middleware.cors import CORSMiddleware
 
 app = GraphQL(schema)
+
+app = CORSMiddleware(
+    app=app,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 if __name__ == "__main__":
     import uvicorn
